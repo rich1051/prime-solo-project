3c2786bae89dbfc1fca4d64a37c653a7bf868fa0
Will Richardson <rich1051@umn.edu>
2023-06-25 15:55:54 -0500

    Created movieListReducer, imported movieListReducer to rootReducer, created search non-functional search form in userPage.jsx

---
+++ src/redux/reducers/movieList.reducer.js
@@ -0,0 +1,12 @@
+const movieListReducer = (state = [], action) => {
+    console.log("action.payload before case type SET_MOVIE is:", action.payload);
+       switch (action.type) {
+           case 'SET_MOVIE':
+                console.log("action.payload BEFORE RETURNING:", action.payload.data);
+               return action.payload.data
+           default:
+       return state
+   }
+   }
+
+export default movieListReducer;
