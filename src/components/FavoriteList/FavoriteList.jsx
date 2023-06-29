20562b3e53b17d28c85a51ea97399c9601f5b073
Will Richardson <rich1051@umn.edu>
2023-06-29 16:31:00 -0500

    FavoriteList working

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -4,55 +4,60 @@ import { useEffect } from "react";
 import ViewRecipeModal from "../ViewRecipeModal/ViewRecipeModal";
 
 function FavoriteList() {
+  const dispatch = useDispatch();
+  const getFavoriteReducer = useSelector((store) => store.getFavoriteReducer);
 
-    const dispatch = useDispatch();
-    const getFavoriteReducer = useSelector((store) => store.getFavoriteReducer);
+  console.log("getFavoriteReducer is:", getFavoriteReducer);
 
+  useEffect(() => {
+    // Fetch recipes when the component mounts
+    getFavorites();
+  }, []);
 
-    useEffect(() => {
-      // Fetch recipes when the component mounts
-      getFavorites();
-    }, []);
-  
-    // THIS GET REQUEST IS CALLED AT THE BEGINNING TO SHOW ALL EXISTING RECIPES IN THE DB:
-    const getFavorites = () => {
-      axios
-        .get("/api/favorites")
-        .then((response) => {
-          // Dispatch an action to update the state with the fetched recipes
-          console.log("response is:", response);
-          dispatch({
-            type: "SET_FAVORITES",
-            payload: response.data,
-          });
-          console.log("response.data is:", response.data);
-        })
-        .catch((error) => {
-          console.log("Error fetching favorites:", error);
+  // THIS GET REQUEST IS CALLED AT THE BEGINNING TO SHOW ALL EXISTING RECIPES IN THE DB:
+  const getFavorites = () => {
+    axios
+      .get("/api/favorites")
+      .then((response) => {
+        // Dispatch an action to update the state with the fetched recipes
+        console.log("response is:", response);
+        dispatch({
+          type: "SET_FAVORITES",
+          payload: response.data,
         });
-    };
+        console.log("response.data is:", response.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching favorites:", error);
+      });
+  };
 
-    const handleRemove = async () => {
-        try {
-          // Update the favorite status in the database
-          await axios.put(`/api/recipes/${id}/favorite`);
-        } catch (error) {
-          console.error("Error updating recipe favorite status:", error);
-        }
-      };
-    
-    return (
-        <div>
-        {getFavoriteReducer.map((recipe) => (
+  const handleRemove = async (recipeId) => {
+    try {
+      // Update the favorite status in the database
+      await axios.put(`/api/recipes/${recipeId}/favorite`);
+      getFavorites();
+    } catch (error) {
+      console.error("Error updating recipe favorite status:", error);
+    }
+  };
+
+  return (
+    <div>
+      {getFavoriteReducer.length === 0 ? (
+        <p>Oh no! You don't have any favorite recipes!</p>
+      ) : (
+        getFavoriteReducer.map((recipe) => (
           <div key={recipe.id}>
             <h4>{recipe.title}</h4>
             <p>Author: {recipe.author}</p>
             <ViewRecipeModal recipe={recipe} />
-            <button onClick={() => handleRemove(recipe)}>Remove</button>
+            <button onClick={() => handleRemove(recipe.id)}>Remove</button>
           </div>
-        ))}
-      </div>
-    )
-};
+        ))
+      )}
+    </div>
+  );
+}
 
-export default FavoriteList
+export default FavoriteList;
